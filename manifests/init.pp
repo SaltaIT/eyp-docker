e025095f1ccef990a2795bf0b91ed1e145579c78
Jordi Prats <jprats@systemadmin.es>
2016-03-10 18:23:56 +0100

    docker-storage-setup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
 #
-class docker ($insecure_registry=undef, $insecure_registry_port="80" )
-  inherits docker::params {
+class docker(
+              $insecure_registry=undef,
+              $insecure_registry_port="80",
+              $devs=undef,
+              $volumegroup=undef,
+            ) inherits docker::params {
 
   #
   package { $docker_package:
@@ -8,14 +12,34 @@ class docker ($insecure_registry=undef, $insecure_registry_port="80" )
   }
 
   file { '/etc/sysconfig/docker':
-    ensure => 'present',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    content => template("${module_name}/sysconfig_docker.erb"),
-    notify => Service['docker'],
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/sysconfig/docker.erb"),
+    notify  => Service['docker'],
     require => Package[$docker_package],
   }
+
+  if($devs!=undef)
+  {
+    file { '/etc/sysconfig/docker-storage-setup':
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template("${module_name}/sysconfig/docker_storage.erb"),
+      notify  => Exec['docker-storage-setup'],
+      require => Package[$docker_package],
+    }
+
+    exec { 'docker-storage-setup':
+      command     => '/usr/bin/docker-storage-setup',
+      refreshonly => true,
+      notify      => Service['docker'],
+    }
+  }
+
 
   service { 'docker':
     ensure => 'running',
