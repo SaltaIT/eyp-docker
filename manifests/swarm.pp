9a92c34737007f2ddea1aad7918b743a6bbbd849
Jordi Prats <jordi.prats@gmail.com>
2019-06-05 09:43:10 +0200

    prova

--- manifests/swarm.pp
+++ manifests/swarm.pp
@@ -1,6 +1,7 @@
 class docker::swarm() inherits docker::params {
 
   #TODO: http://blog.vpetkov.net/2015/12/07/docker-swarm-tutorial-and-examples/
+
   Exec {
     path => '/usr/sbin:/usr/bin:/sbin:/bin',
   }
@@ -10,6 +11,6 @@ class docker::swarm() inherits docker::params {
     unless  => 'docker images | grep \'docker.io/swarm\'',
   }
 
-  fail('not implemented')
+  fail('TO BE IMPLEMENTED')
 
 }
