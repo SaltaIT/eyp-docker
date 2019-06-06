5736976ece66292e36156b8dc0a860a85c6582ac
Jordi Prats <jordi.prats@gmail.com>
2019-06-06 11:15:10 +0200

    typo

--- manifests/cerepo.pp
+++ manifests/cerepo.pp
@@ -3,6 +3,6 @@ class docker::cerepo() inherits docker::params {
     ensure  => 'present',
     descr   => 'Docker Community Edition repository for CentOS',
     baseurl => 'https://download.docker.com/linux/centos/7/x86_64/stable',
-    enabled => 'true'
+    enabled => true,
   }
 }
