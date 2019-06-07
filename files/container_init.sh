f10d4041a5c1c6404adeac3276c8392e04e2836d
Jordi Prats <jordi.prats@gmail.com>
2019-06-07 13:02:19 +0200

    to full init script

--- files/container_init.sh
+++ files/container_init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# chkconfig: 345 20 99
+# description: container init script
+#. /etc/rc.d/init.d/functions
+# Default-Start: 3 5
+# Default-Stop: 0 6
 
 if [ ! -e "/usr/bin/which" ];
 then
@@ -21,21 +26,46 @@ then
   exit 3
 fi
 
-docker inspect $CONTAINER_ID >/dev/null 2>&1
+status()
+{
+  docker inspect $CONTAINER_ID >/dev/null 2>&1
 
-if [ "$?" -ne 0 ];
-then
-  echo "container ${CONTAINER_ID} does not exists"
-  exit 2
-fi
+  if [ "$?" -ne 0 ];
+  then
+    echo "container ${CONTAINER_ID} does not exists"
+    exit 2
+  fi
 
-docker ps | grep "${CONTAINER_ID}" >/dev/null 2>&1
+  docker ps | grep "${CONTAINER_ID}" >/dev/null 2>&1
 
-if [ "$?" -eq 0 ];
-then
-  echo "container ${CONTAINER_ID} is running"
+  if [ "$?" -eq 0 ];
+  then
+    echo "container ${CONTAINER_ID} is running"
+    exit 0
+  else
+    echo "container ${CONTAINER_ID} is NOT running"
+    exit 2
+  fi
+}
+
+case $1 in
+  'start')
+  docker start ${CONTAINER_ID}
   exit 0
-else
-  echo "container ${CONTAINER_ID} is NOT running"
-  exit 2
-fi
+  ;;
+  'stop')
+  docker stop ${CONTAINER_ID}
+  exit 0
+  ;;
+  'restart')
+    $0 stop
+    $0 start
+  ;;
+  'status')
+  status()
+  ;;
+  *)
+  echo "$0 $1: unrecognized command"
+  exit 1
+  ;;
+esac
