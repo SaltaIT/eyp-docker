b3250bfb35f3198d65976cf7f76e750733e8b84b
Jordi Prats <jordi.prats@gmail.com>
2019-11-06 16:14:51 +0100

    added cis related options

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,20 @@
 #
 class docker(
-              $manage_package        = true,
-              $package_ensure        = 'installed',
-              $manage_service        = true,
-              $manage_docker_service = true,
-              $service_ensure        = 'running',
-              $service_enable        = true,
-              $install_nagios_checks = true,
-              $storage_driver        = 'overlay2',
+              $manage_package                = true,
+              $package_ensure                = 'installed',
+              $manage_service                = true,
+              $manage_docker_service         = true,
+              $service_ensure                = 'running',
+              $service_enable                = true,
+              $install_nagios_checks         = true,
+              $storage_driver                = 'overlay2',
+              $inter_container_communication = false,
+              $log_level                     = 'info',
+              $live_restore                  = true,
+              $iptables                      = true,
+              $userland_proxy                = false,
+              $no_new_privileges             = true,
+              $bridge_ip                     = undef,
             ) inherits docker::params {
 
   class { '::docker::install': } ->
